84c416528565ae5d465da535d46e47201b68aeb6
jmgarciah <jgarciah@ing.uc3m.es>
2016-11-08 16:15:48 +0100

    generated the waiter setDictionary

--- programs/waiterDialogueManager/WaiterDialogueManager.cpp
+++ programs/waiterDialogueManager/WaiterDialogueManager.cpp
@@ -28,35 +28,48 @@ bool WaiterDialogueManager::configure(yarp::os::ResourceFinder &rf) {
     outCmdPortHead.open("/waiterDialMan/Manip/command:o");
     outCmdPortManip.open("/waiterDialMan/Head/command:o");
     outTtsPort.open("/waiterDialMan/tts/rpc:c");
+    outRecognitionPort.open("/waiterDialMan/speechRecognition/setDictionary/rpc:c");
     inSrPort.open("/waiterDialMan/speechRecognition:i");
     stateMachine.setOutCmdPortHead(&outCmdPortHead);
     stateMachine.setOutCmdPortManip(&outCmdPortManip);
     stateMachine.setOutTtsPort(&outTtsPort);
     stateMachine.setInSrPort(&inSrPort);
+    yarp::os::Network::connect("/waiterDialMan/speechRecognition/setDictionary/rpc:c", "/speechRecognition/rpc:s");
+
     while(1){
         if(outTtsPort.getOutputCount() > 0) break;
         printf("Waiting for '/waiterDialMan/tts/rpc:c' to be connected to something...\n");
         yarp::os::Time::delay(0.5);
     }
-    yarp::os::Bottle bOut, bRes;
+    yarp::os::Bottle bOut,  bRec;
+
     bOut.addString("setLanguage");
+    bRec.addString("setDictionary");
+    bRec.addString("waiter");
+
     if( language == "english" )
     {
         bOut.addString("mb-en1");
+        bRec.addString(language );
     }
     else if ( language == "spanish" )
     {
         bOut.addString("mb-es1");
+        bRec.addString(language);
     }
     else
     {
         printf("Language not found. Please use '--language english' or '--language spanish'");
         return false;
     }
-    outTtsPort.write(bOut,bRes);
+    printf("%s\n", bOut.toString().c_str());
+    outTtsPort.write(bOut);
+    printf("%s\n", bRec.toString().c_str());
+    outRecognitionPort.write(bRec);
 
     stateMachine.setLanguage(language);
     stateMachine.start();
+
     return true;
 }
 
